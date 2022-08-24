ca01da6ade315b0ec74e949898a4da34f1000135
lars <larskaesberg@yahoo.de>
2022-08-24 22:22:02 +0200

    feat(graph)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend,
+} from 'chart.js';
+import {Line} from 'react-chartjs-2';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+);
+const fetch = require('sync-fetch')
+const lars_data = fetch("https://studip.larskaesberg.de/logs/l.kaesberg.log").text()
+const constantin_data = fetch("https://studip.larskaesberg.de/logs/c.dalinghaus.log").text()
+
+
+export const options = {
+    responsive: true,
+    interaction: {
+        mode: 'index',
+        intersect: false,
+    },
+    stacked: false,
+    plugins: {
+        title: {
+            display: true,
+            text: 'StudIP - Score',
+        },
+    },
+    scales: {
+        y: {
+            type: 'linear',
+            display: true,
+            position: 'left',
+        },
+        x: {
+            type: 'linear',
+            display: true,
+            grid: {
+                drawOnChartArea: false,
+            },
+        },
+    },
+};
+
+
+export const data = {
+    datasets: [
+        {
+            label: 'l.kaesberg',
+            data: lars_data.split("\n").map(line => {
+                const data = line.split(" ")
+                return {x: data[1], y: data[0]}
+            }),
+            borderColor: 'rgb(255, 99, 132)',
+            backgroundColor: 'rgba(255, 99, 132, 0.5)',
+        },
+        {
+            label: 'c.dalinghaus',
+            data: constantin_data.split("\n").map(line => {
+                const data = line.split(" ")
+                return {x: data[1], y: data[0]}
+            }),
+            borderColor: 'rgb(53, 162, 235)',
+            backgroundColor: 'rgba(53, 162, 235, 0.5)',
+        },
+    ],
+};
+
+export function App() {
+    return <Line options={options} data={data}/>;
 }
-
-export default App;
