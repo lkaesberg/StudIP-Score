9b78ba85a8dcb8eec6a5e6e1d68fec55df5e2aee
l.kaesberg <l.kaesberg@stud.uni-goettingen.de>
2022-09-12 20:05:30 +0200

    feat(zoom): add zoom feature to chart

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import './App.css';
 import 'chartjs-adapter-date-fns';
 import {
@@ -12,7 +12,10 @@ import {
     Tooltip,
     Legend, Chart,
 } from 'chart.js';
+import zoomPlugin, {resetZoom} from 'chartjs-plugin-zoom';
 import {Line} from 'react-chartjs-2';
+import {useState, useEffect} from 'react';
+import Button from '@mui/material/Button';
 
 ChartJS.register(
     CategoryScale,
@@ -22,7 +25,8 @@ ChartJS.register(
     LineElement,
     Title,
     Tooltip,
-    Legend
+    Legend,
+    zoomPlugin
 );
 const stc = require('string-to-color');
 const fetch = require('sync-fetch')
@@ -36,9 +40,31 @@ function split_data(data) {
     })
 }
 
+function getWindowDimensions() {
+    const {innerWidth: width, innerHeight: height} = window;
+    return {
+        width,
+        height
+    };
+}
+
+export default function useWindowDimensions() {
+    const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+
+    useEffect(() => {
+        function handleResize() {
+            setWindowDimensions(getWindowDimensions());
+        }
+
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
+    return windowDimensions;
+}
 const users = ["l.kaesberg", "c.dalinghaus", "s.kampen", "niklas.bauer01", "hbrosen"]
 const user_data = users.map(user => fetch(`https://gwdg.larskaesberg.de/logs/${user}.log`).text())
-const user_data_array = user_data.map(data => split_data(data).filter((_, i, array) => i%Math.ceil(array.length / 100) == 0 || i == array.length -1))
+const user_data_array = user_data.map(data => split_data(data).filter((_, i, array) => i % Math.ceil(array.length / 100) === 0 || i === array.length - 1))
 const user_last_value = user_data_array.map(data => data[data.length - 2]["y"])
 
 
@@ -46,7 +72,8 @@ export const options = {
     responsive: true,
     maintainAspectRatio: false,
     interaction: {
-        mode: 'x',
+        mode: "nearest",
+        axis: "x",
         intersect: false,
     },
     stacked: false,
@@ -62,6 +89,14 @@ export const options = {
                 size: 30
             },
             color: "white"
+        },
+        zoom: {
+            zoom: {
+                drag: {
+                    enabled: true
+                },
+                mode: 'x',
+            },
         },
         legend: {
             display: true,
@@ -134,9 +169,13 @@ export const data = {
 };
 
 export function App() {
+    const chartRef = useRef(null);
     return (
         <header className="App-header">
-            <Line className="chart" options={options} data={data}/>
+            <Line ref={chartRef} className="chart" options={options} data={data}/>
+            <div className={"chart-buttons"}>
+                <Button onClick={() => chartRef.current.resetZoom()} variant="Contained">Reset Zoom</Button>
+            </div>
             <br/>
             <div>Aktuell
                 Erster: {users[user_last_value.indexOf(Math.max(...user_last_value))]} ({Math.max(...user_last_value)} Punkte)
